7f982a2926306a966ee1dba33b2269435053ddef
lifeNewbi <lastchallenged@gmail.com>
2022-08-10 13:28:12 +0900

    .env 수정

--- database/mongodbConnection.js
+++ database/mongodbConnection.js
@@ -1,32 +1,10 @@
-// const mongoose = require("mongoose");
-// const { MongoClient } = require("mongodb");
-// require("dotenv").config();
-
-// const mongoConnection = () => {
-//   mongoose
-//     .connect(process.env.MONGO_URL, {
-//       // useCreateIndex: true,
-//       // useFindAndModify: false,
-//     })
-//     .then(() => console.log("MongoDB conected"))
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// };
-
-// const mongoClient = () => {
-//   const client = new MongoClient(process.env.MONGO_URL);
-//   return client;
-// };
-
-// heroku Config vars 변경
 const mongoose = require("mongoose");
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const mongoConnection = () => {
   mongoose
-    .connect(process.env.PORT, {
+    .connect(process.env.MONGO_URL, {
       // useCreateIndex: true,
       // useFindAndModify: false,
     })
@@ -37,8 +15,30 @@ const mongoConnection = () => {
 };
 
 const mongoClient = () => {
-  const client = new MongoClient(process.env.PORT);
+  const client = new MongoClient(process.env.MONGO_URL);
   return client;
 };
+
+// heroku Config vars 변경
+// const mongoose = require("mongoose");
+// const { MongoClient } = require("mongodb");
+// require("dotenv").config();
+
+// const mongoConnection = () => {
+//   mongoose
+//     .connect(process.env.PORT, {
+//       // useCreateIndex: true,
+//       // useFindAndModify: false,
+//     })
+//     .then(() => console.log("MongoDB conected"))
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// };
+
+// const mongoClient = () => {
+//   const client = new MongoClient(process.env.PORT);
+//   return client;
+// };
 
 module.exports = { mongoConnection, mongoClient };
